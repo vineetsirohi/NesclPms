5cb1c2287c23e5c3ad863ae56a9270e32c0fa1b5
vineetsirohi <vineetksirohi@gmail.com>
2014-10-10 10:58:14 +0530

    database initial seed

--- NesclPms.WebUI/Infrastructure/AppIdentityDbContext.cs
+++ NesclPms.WebUI/Infrastructure/AppIdentityDbContext.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity;
 
 namespace NesclPms.WebUI.Infrastructure
 {
@@ -37,6 +38,30 @@ namespace NesclPms.WebUI.Infrastructure
         public void PerformInitialSetup(AppIdentityDbContext context)
         {
             // initial configuration will go here
+            AppUserManager userMgr = new AppUserManager(new UserStore<AppUser>(context));
+            AppRoleManager roleMgr = new AppRoleManager(new RoleStore<AppRole>(context));
+
+            string roleName = "admin";
+            string userName = "Admin";
+            string password = "vinsir";
+            string email = "vineetsirohi01@gmail.com";
+
+            if (!roleMgr.RoleExists(roleName))
+            {
+                roleMgr.Create(new AppRole(roleName));
+            }
+
+            AppUser user = userMgr.FindByName(userName);
+            if (user == null)
+            {
+                userMgr.Create(new AppUser { UserName = userName, Email = email },
+                    password);
+                user = userMgr.FindByName(userName);
+            }
+            if (!userMgr.IsInRole(user.Id, roleName))
+            {
+                userMgr.AddToRole(user.Id, roleName);
+            }
         }
     }
 }
