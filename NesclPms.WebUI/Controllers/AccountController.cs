8f33fa1a24f1560032cfa46cf2c987bcd360b7d6
vineetsirohi <vineetksirohi@gmail.com>
2014-11-20 17:11:25 +0530

    User can change passwords

--- NesclPms.WebUI/Controllers/AccountController.cs
+++ NesclPms.WebUI/Controllers/AccountController.cs
@@ -78,5 +78,36 @@ namespace NesclPms.WebUI.Controllers
                 return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
             }
         }
+
+        internal class ChallengeResult : HttpUnauthorizedResult
+        {
+            private const string XsrfKey = "XsrfId";
+
+            public ChallengeResult(string provider, string redirectUri)
+                : this(provider, redirectUri, null)
+            {
+            }
+
+            public ChallengeResult(string provider, string redirectUri, string userId)
+            {
+                LoginProvider = provider;
+                RedirectUri = redirectUri;
+                UserId = userId;
+            }
+
+            public string LoginProvider { get; set; }
+            public string RedirectUri { get; set; }
+            public string UserId { get; set; }
+
+            public override void ExecuteResult(ControllerContext context)
+            {
+                var properties = new AuthenticationProperties { RedirectUri = RedirectUri };
+                if (UserId != null)
+                {
+                    properties.Dictionary[XsrfKey] = UserId;
+                }
+                context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
+            }
+        }
     }
 }
--- NesclPms.WebUI/Models/AppUser.cs
+++ NesclPms.WebUI/Models/AppUser.cs
@@ -3,11 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
 
 namespace NesclPms.WebUI.Models
 {
     public class AppUser : IdentityUser
     {
         public string LabelName { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<AppUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
+
+    
 }
